fa4bdb5ac5c1f1d1b270674a52cd10066787264f
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-03 23:35:56 +0300

    Create DegiskenlerVeMatematikselOpOrnek.java

---
+++ javaWorld/src/MatematikselOperatorler/DegiskenlerVeMatematikselOpOrnek.java
@@ -0,0 +1,18 @@
+package MatematikselOperatorler;
+
+public class DegiskenlerVeMatematikselOpOrnek {
+
+	public static void main(String[] args) {
+		
+		int x = 9, y = 4, z;
+		
+		System.out.println("x = " + x + " <-> " + "y = "+ y);
+		z = y; // z-> 4;
+		y = x; // y-> 9;
+		x = z; // x-> 4;
+		System.out.println("x = " + x + " <-> " + "y = "+ y);
+		
+
+	}
+
+}
