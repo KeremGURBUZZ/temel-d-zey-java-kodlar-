12a7d5a7ae4ce2b9c25739a63a87db1565625f0c
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-03 23:35:49 +0300

    Create Degiskenler01.java

---
+++ javaWorld/src/Degiskenler/Degiskenler01.java
@@ -0,0 +1,21 @@
+package Degiskenler;
+
+public class Degiskenler01 {
+
+	public static void main(String[] args) {
+		
+		int x = 15;
+		System.out.println(x); // int tam sayıları tanımlamak için kullanılır - + 8 basamağa kadar depolanabilir.
+		
+		double y = 10.5;
+		System.out.println(y); // double ondalıklı sayılar için kullanılır
+		
+		char kerem = '*';
+		System.out.println(kerem); // char sadece 1 karakteri tutar '' tek tırnakla tanımlanır.
+		
+		String metin = "Kerem Gürbüz junior yazılımcıdır.";
+		System.out.println(metin); // String baş harfi büyük yazılır tanımlanırken "" çift tırnakla yazılır
+
+	}
+
+}
