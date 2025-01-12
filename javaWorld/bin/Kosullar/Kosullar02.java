63761efd82c8c5b8de18071c79dfba26e266dee9
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-12 23:39:08 +0300

    Create Kosullar02.java

---
+++ javaWorld/bin/Kosullar/Kosullar02.java
@@ -0,0 +1,34 @@
+package Kosullar;
+
+public class Kosullar02 {
+
+	public static void main(String[] args) {
+		
+		
+		int x = -5;
+		if(x<0 || x<10 ) 
+		{
+			System.out.println("Burası çalıştı.");
+		}
+		
+		
+		
+		
+		
+		
+		// && ---> ve
+		// || ---> veya 
+		
+	
+		
+		/*
+		int x = 5;
+		if(x>0 && x<10) 
+		{
+			System.out.println("0'dan büyük.");
+			System.out.println("1'den büyük.");
+		}
+		*/
+		
+   }
+}
