aa77c7c690f765b28631d82f975dcdc12eab03bb
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-12 23:39:19 +0300

    Create Kosullar04.java

---
+++ javaWorld/bin/Kosullar/Kosullar04.java
@@ -0,0 +1,35 @@
+package Kosullar;
+
+public class Kosullar04 {
+
+	public static void main(String[] args) {
+		
+		 int x = 100;
+		 
+		 if(x < 10) 
+		 {
+			 System.out.println("x' 10'dan küçük.");
+		 }
+		 else if(x < 20) 
+		 {
+			System.out.println("x' 20'den küçük.");
+		 }
+		 else if(x < 30)
+		 {
+			 System.out.println("x' 30'dan küçük.");
+		 }else if(x < 40) 
+		 {
+			System.out.println("x' 40'tan küçük.");
+		 }
+		 else if(x < 50)
+		 {
+			 System.out.println("x' 50'den küçük.");
+		 }else 
+		 {
+			 System.out.println("Hiçbirine uymuyor!");
+		 }
+		
+
+	}
+
+}
