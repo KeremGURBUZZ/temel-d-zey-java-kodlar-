a026e7459e684e3d0c83b5af610cc09a80bf930a
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-12 23:39:02 +0300

    Create Kosullar01.java

---
+++ javaWorld/bin/Kosullar/Kosullar01.java
@@ -0,0 +1,18 @@
+package Kosullar;
+
+public class Kosullar01 {
+
+	public static void main(String[] args) {
+		
+		int x = 5;
+		if(x>0) 
+		{
+			System.out.println("0'dan büyük.");
+			System.out.println("1'den büyük.");
+		}
+/*
+ * if(kosul){işlem}türkçe karşılığı gibi eğer koşul sağlanıyorsa işlemi yap  
+ */
+	}
+
+}
