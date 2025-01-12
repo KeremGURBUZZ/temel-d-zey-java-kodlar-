00281d9d4ed6f70bff496f6e608e6fd872901cef
KeremGURBUZZ <keremgurbuz5@gmail.com>
2025-01-12 23:39:14 +0300

    Create Kosullar03.java

---
+++ javaWorld/bin/Kosullar/Kosullar03.java
@@ -0,0 +1,24 @@
+package Kosullar;
+
+public class Kosullar03 {
+
+	public static void main(String[] args) {
+		
+		int x = 55;
+        if(x<10) 
+        {
+        	System.out.println("x'küçük bir sayı");
+        }
+        else 
+        {
+        	System.out.println("x'büyük bir sayı.");
+        }
+        /*
+         * if-eğer koşul sağlanıyorsa 
+         * else if ek koşul sağlanıyorsa
+         * else aksi taktirde şu işlemi yap şeklinde çalışır. 
+         */
+
+	}
+
+}
